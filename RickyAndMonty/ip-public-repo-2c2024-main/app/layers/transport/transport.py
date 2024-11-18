e6e5e308c44c2e5e518482b187bd4f5ae9475637
LucasIbarra03 <lucasgibarra2003@gmail.com>
2024-11-18 18:07:48 -0300

    Modificaciones transport.py
    Modificaciones para que funcione la función "def getAllImages"

--- RickyAndMonty/ip-public-repo-2c2024-main/app/layers/transport/transport.py
+++ RickyAndMonty/ip-public-repo-2c2024-main/app/layers/transport/transport.py
@@ -1,32 +1,69 @@
-# capa de transporte/comunicación con otras interfaces o sistemas externos.
+# capa de servicio/lógica de negocio
 
-import requests
-from ...config import config
-
-# comunicación con la REST API.
-# este método se encarga de "pegarle" a la API y traer una lista de objetos JSON crudos (raw).
+from ..persistence import repositories
+from ..utilities import translator
+from django.contrib.auth import get_user
+import requests 
 def getAllImages(input=None):
-    if input is None:
-        json_response = requests.get(config.DEFAULT_REST_API_URL).json()
+    url = "https://rickandmortyapi.com/api/character/" # URL de la API de Rick & Morty para obtener personajes
+
+    response = requests.get(url)  # Realizamos la solicitud GET a la API
+
+    if response.status_code == 200: # Verificamos si la solicitud fue exitosa
+        data = response.json()  # Convertimos la respuesta en formato JSON
+
+        images = []
+        for character in data['results']: # Iteramos sobre la lista de personajes en la respuesta de la API
+            episode_url = character['episode'][0] # Obtenemos la URL del primer episodio del personaje
+            
+            # Realizamos una solicitud adicional para obtener los datos del episodio
+            episode_response = requests.get(episode_url)
+            episode_data = episode_response.json()
+            
+            # Obtenemos el nombre del episodio inicial
+            episode_name = episode_data['name']  
+
+            # Obtenemos la última ubicación del personaje
+            last_location = character['location']['name']
+
+            # Agregamos la informacion del personaje
+            character_data = {
+                'name': character['name'],  # Nombre del personaje
+                'image': character['image'],  # URL de la imagen
+                'status': character['status'],  # Estado del personaje (Alive, Dead, etc.)
+                'last_location': last_location,  # Última ubicación conocida
+                'episode_name': episode_name,  # Nombre del primer episodio donde aparece
+            }
+            
+            # Agregamos el personaje a la lista
+            images.append(character_data)
+
+        return images
+
+
+# añadir favoritos (usado desde el template 'home.html')
+def saveFavourite(request):
+    fav = '' # transformamos un request del template en una Card.
+    fav.user = '' # le asignamos el usuario correspondiente.
+
+    return repositories.saveFavourite(fav) # lo guardamos en la base.
+
+# usados desde el template 'favourites.html'
+def getAllFavourites(request):
+    if not request.user.is_authenticated:
+        return []
     else:
-        json_response = requests.get(config.DEFAULT_REST_API_SEARCH + input).json()
+        user = get_user(request)
 
-    json_collection = []
+        favourite_list = [] # buscamos desde el repositories.py TODOS los favoritos del usuario (variable 'user').
+        mapped_favourites = []
 
-    # si la búsqueda no arroja resultados, entonces retornamos una lista vacía de elementos.
-    if 'error' in json_response:
-        print("[transport.py]: la búsqueda no arrojó resultados.")
-        return json_collection
+        for favourite in favourite_list:
+            card = '' # transformamos cada favorito en una Card, y lo almacenamos en card.
+            mapped_favourites.append(card)
 
-    for object in json_response['results']:
-        try:
-            if 'image' in object:  # verificar si la clave 'image' está presente en el objeto (sin 'image' NO nos sirve, ya que no mostrará las imágenes).
-                json_collection.append(object)
-            else:
-                print("[transport.py]: se encontró un objeto sin clave 'image', omitiendo...")
+        return mapped_favourites
 
-        except KeyError: 
-            # puede ocurrir que no todos los objetos tenga la info. completa, en ese caso descartamos dicho objeto y seguimos con el siguiente en la próxima iteración.
-            pass
-
-    return json_collection
+def deleteFavourite(request):
+    favId = request.POST.get('id')
+    return repositories.deleteFavourite(favId) # borramos un favorito por su ID.
